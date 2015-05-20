e252c711c0bcf7777b39a9c309c0a05c5fc85752
lnmnd <lnmnd@users.noreply.github.com>
2015-05-20 19:04:12 +0200

    self-evaluating: boolean (true/false)

--- eval.scm
+++ eval.scm
@@ -3,6 +3,14 @@
   (import chicken scheme)
   (import (only data-structures conc))
 
+  (define (lboolean? exp)
+    (and (symbol? exp)
+	 (or (eq? 'true exp) (eq? 'false exp))))
+  
+  (define (self-evaluating? exp)
+    (cond ((lboolean? exp) #t)
+	  (else #f)))
+  
   (define (tagged-list? exp tag)
     (if (pair? exp)
 	(eq? (car exp) tag)
@@ -31,7 +39,8 @@
       value))  
 
   (define (lisp-eval exp env)
-    (cond ((quoted? exp) (quoted-exp exp))
+    (cond ((self-evaluating? exp) exp)
+	  ((quoted? exp) (quoted-exp exp))
 	  ((definition? exp) (eval-definition exp env))
 	  ((reset? exp) (eval-reset exp env))
 	  ((symbol? exp) (env 'get exp))
