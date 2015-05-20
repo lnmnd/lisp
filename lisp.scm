502e0b7ac3dada3f5fde3aa7fa234737834983b3
lnmnd <lnmnd@users.noreply.github.com>
2015-05-20 11:46:32 +0200

    repl: always print result, no 'nothing'

--- lisp.scm
+++ lisp.scm
@@ -8,36 +8,31 @@
 (define (second l) (cadr l))
 (define (third l) (caddr l))
 
-(define (nothing? x)
-  (and (symbol? x) (eq? x 'nothing)))
-
 ;; do not eval, pass
 (define (lisp-eval exp env)
   exp)
 
 ;; Read Eval Print Loop
-(define (repl env display-prompt)
+(define (repl env)
   ;; if lisp exception print exception
   ;; else abort
   (handle-exceptions
       exn
       (cond ((and (list? exn) (eq? (first exn) 'lisp))
 	     (begin (print (second exn))
-		    (repl env display-prompt)))
+		    (repl env)))
 	    (else (abort exn)))
 
     (begin
-      (if display-prompt (display "> "))
+      (display "> ")
       (let ((exp (with-input-from-port (current-input-port) read)))
 	(if (eof-object? exp)
 	    (print "exit")
 	    (begin
 	      (let ((evaluated-exp (lisp-eval exp env)))
-		(if (nothing? evaluated-exp)
-		    (repl env #f)
-		    (begin (write evaluated-exp)
-			   (newline)
-			   (repl env #t))))))))))
+		(begin (write evaluated-exp)
+		       (newline)
+		       (repl env)))))))))
 
 (let ((env (make-env #f)))
-  (repl env #t))
+  (repl env))
