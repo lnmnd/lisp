5c624acedb81d55b12de18b1b45130e4a81f0257
lnmnd <lnmnd@users.noreply.github.com>
2015-05-20 18:30:29 +0200

    eval: quote

--- eval.scm
+++ eval.scm
@@ -1,9 +1,22 @@
 (module eval
     (lisp-eval)
   (import chicken scheme)
+  (import (only data-structures conc))
+
+  (define (tagged-list? exp tag)
+    (if (pair? exp)
+	(eq? (car exp) tag)
+	#f))
+
+  (define (quoted? exp)
+    (tagged-list? exp 'quote))
+
+  (define (quoted-exp exp)
+    (cadr exp))
 
   ;; do not eval, pass
   (define (lisp-eval exp env)
-    exp)
+    (cond ((quoted? exp) (quoted-exp exp))
+	  (else (abort `(lisp ,(conc "cannot evaluate " exp))))))
   
   )
