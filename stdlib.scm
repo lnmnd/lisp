07aca656eb4713c9e7101b4aa2e5fe88a8fb282c
lnmnd <lnmnd@users.noreply.github.com>
2015-06-13 11:26:56 +0200

    some functions related to lists

--- stdlib.scm
+++ stdlib.scm
@@ -32,4 +32,32 @@
 	       (cons (first (rest args))
 		     (rest (rest args))))))
 
+;; lists
+
 (defn list xs xs)
+
+(defn second (xs)
+  (first (rest xs)))
+
+(defn reverse (xs)
+  (defn reverse-it (acc xs)
+    (if (empty? xs)
+	acc
+	(reverse-it (cons (first xs) acc) (rest xs))))
+  (reverse-it (list) xs))
+
+(defn filter (pred xs)
+  (defn filter-it (acc pred xs)
+    (if (empty? xs)
+	acc
+	(if (pred (first xs))
+	    (filter-it (cons (first xs) acc) pred (rest xs))
+	    (filter-it acc pred (rest xs)))))
+  (filter-it (list) pred (reverse xs)))
+
+(defn map (f xs)
+  (defn map-it (acc f xs)
+    (if (empty? xs)
+	acc
+	(map-it (cons (f (first xs)) acc) f (rest xs))))
+  (map-it (list) f (reverse xs)))
